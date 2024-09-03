a227eb437b48bbd530030cea1c4bda3a1645c396
zezikvl <zezik.vl@gmail.com>
2024-09-03 16:28:56 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+#2
+my_string = input('Введите Ваше ФИО ')
+print(len(my_string))
+
+#3
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
